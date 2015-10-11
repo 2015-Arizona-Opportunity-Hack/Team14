40aa98564c7b9367eb356d9c2cc639fb77fd4de3
r-dc <chhristian.robles@gmail.com>
2015-10-10 22:49:27 -0700

    working on fabric gradle

--- app/src/main/java/com/ohack/icanalert/Feed.java
+++ app/src/main/java/com/ohack/icanalert/Feed.java
@@ -1,13 +1,20 @@
 package com.ohack.icanalert;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.tweetui.TweetUi;
 import com.twitter.sdk.android.tweetui.UserTimeline;
 import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
+
+import io.fabric.sdk.android.Fabric;
 
 public class Feed extends ListActivity {
 
@@ -15,13 +22,16 @@ public class Feed extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        Intent intent = getIntent();
+
+        TwitterAuthConfig authConfig =  new TwitterAuthConfig("5619c235548c2bc0700001d0", "33fc324dfbb936aea3015f8524108eda9d59075d30da3a7def4bc8553e3dd289");
+        Fabric.with(this, new TwitterCore(authConfig), new TweetUi());
 
         final UserTimeline userTimeline = new UserTimeline.Builder()
                 .screenName("chrisrobeless")
                 .build();
-
         final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(this)
                 .setTimeline(userTimeline)
                 .build();
