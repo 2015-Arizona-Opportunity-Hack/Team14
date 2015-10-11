d785b8a7638dab9ae2534d873eae48dcb1e9c7fc
Aritro <aritro6@gmail.com>
2015-10-11 01:12:09 -0700

    Merge branch 'master' of https://github.com/Opportunity-Hack-2015-Arizona/Team14
    Conflicts:
    	.idea/misc.xml
    	.idea/modules.xml
    	app/src/main/res/layout/activity_language_select.xml

--- app/src/main/java/com/ohack/icanalert/Feed.java
+++ app/src/main/java/com/ohack/icanalert/Feed.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.twitter.sdk.android.core.TwitterAuthConfig;
@@ -29,7 +28,7 @@ public class Feed extends ListActivity {
         TwitterAuthConfig authConfig =  new TwitterAuthConfig("Y9UIuPZDC9ijnHxmvx3G7qdKl", "g9bE72th1SHQDANtVK1wCOhYwwnPJgU4gZEhP9wJkrAYyzBPBt");
         Fabric.with(this, new TwitterCore(authConfig), new TweetUi());
 
-        Intent intent = getIntent();
+
 
         final UserTimeline userTimeline = new UserTimeline.Builder()
                 .screenName("chrisrobeless")
@@ -38,15 +37,6 @@ public class Feed extends ListActivity {
                 .setTimeline(userTimeline)
                 .build();
         setListAdapter(adapter);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
