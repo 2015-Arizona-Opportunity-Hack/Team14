e68d0ee41a801cc422d933a3ad61113753d7521c
r-dc <chhristian.robles@gmail.com>
2015-10-11 08:54:10 -0700

    :

--- app/src/main/java/com/ohack/icanalert/CalendarActivity.java
+++ app/src/main/java/com/ohack/icanalert/CalendarActivity.java
@@ -16,6 +16,21 @@ public class CalendarActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+<<<<<<< HEAD:app/src/main/java/com/ohack/icanalert/CalendarActivity.java
         setContentView(R.layout.content_calendar);
+=======
+        setContentView(R.layout.activity_alerts);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+>>>>>>> parent of 0296f0f... A little styling!:app/src/main/java/com/ohack/icanalert/Alerts.java
     }
 }
