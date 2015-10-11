db03eeaf4bb0d4a59a313b3be50f74940b01a155
r-dc <chhristian.robles@gmail.com>
2015-10-11 08:28:21 -0700

    WHY

--- app/src/main/java/com/ohack/icanalert/Alerts.java
+++ app/src/main/java/com/ohack/icanalert/Alerts.java
@@ -13,10 +13,13 @@ public class Alerts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alerts);
+<<<<<<< HEAD
 
 
 
 
+=======
+>>>>>>> parent of 372e5b7... alerts
     }
 
 }
